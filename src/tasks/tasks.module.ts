e9f7cf1e311cb60cdfb2834123e7cc7ed31780e0
MohamedAli09 <mohamed.ali00870@gmail.com>
2024-12-02 03:31:26 +0200

    add JWT strategy and user decorator for authentication; integrate AuthModule in TasksModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,9 +5,10 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskEntity } from './task.entity';
 import { TasksRepository } from './tasks.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskEntity])],
+  imports: [TypeOrmModule.forFeature([TaskEntity]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TasksRepository],
 })
